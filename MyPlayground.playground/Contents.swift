30ea2746961174d9e1bee28c5ef1272138de253f
常鑫亮 <18270820269@163.com>
2020-09-11 16:24:12 +0800

    49. 字母异位词分组

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -814,3 +814,48 @@ func permute(_ nums: [Int]) -> [[Int]] {
     return result
 }
 //permute([1,2,3])
+
+
+/**
+ 49. 字母异位词分组
+ 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ 示例:
+ 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ 输出:
+ [
+   ["ate","eat","tea"],
+   ["nat","tan"],
+   ["bat"]
+ ]
+ 说明：
+ 所有输入均为小写字母。
+ 不考虑答案输出的顺序。
+ */
+func groupAnagrams(_ strs: [String]) -> [[String]] {
+    var dic = [String:[String]]()
+    func isSame(a: String, b: String) -> Bool {
+        if a.count != b.count {
+            return false
+        }
+        let sortedA = String(a.sorted())
+        let sortedB = String(b.sorted())
+        return sortedA == sortedB
+    }
+    func dealwith(str: String) {
+        
+        let sortedStr = String(str.sorted())
+        guard let sArray = dic[sortedStr] else {
+            dic[sortedStr] = [str]
+            return
+        }
+        var array = sArray
+        array.append(str)
+        dic[sortedStr] = array
+    }
+    
+    for str in strs {
+        dealwith(str: str)
+    }
+    return Array(dic.values)
+}
+groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
